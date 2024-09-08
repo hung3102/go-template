b98e15e4de5501adc9bc09fa1733b338a5d34d4d
54m <30588003+54m@users.noreply.github.com>
2024-09-09 04:52:13 +0900

    update(back):  Event model to use time.Time for BillingMonth
    Replaced the BillingMonth type from string to time.Time in the Event struct. This enhances type safety and ensures proper date management within the application.

--- back/app/internal/volcago/models/event.go
+++ back/app/internal/volcago/models/event.go
@@ -1,11 +1,13 @@
 package models
 
+import "time"
+
 //go:generate ../../../../../bin/volcago -p repositories -o ../repositories -mockgen ../../../../../bin/mockgen -mock-output mocks/event_gen.go Event
 
 // Event - イベント
 type Event struct {
-	ID           string `firestore:"-" firestore_key:"auto"` // ID
-	BillingMonth string ``                                   // 請求月 (YYYYMM)
-	Status       string ``                                   // ステータス
+	ID           string    `firestore:"-" firestore_key:"auto"` // ID
+	BillingMonth time.Time ``                                   // 請求月 (YYYYMM)
+	Status       string    ``                                   // ステータス
 	Meta
 }
