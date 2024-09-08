f8dfa0381346de55df15097b60b6981bc38f34f0
54m <30588003+54m@users.noreply.github.com>
2024-09-09 02:17:40 +0900

    add(back): app

---
+++ back/app/go.mod
@@ -0,0 +1,5 @@
+module github.com/topgate/gcim-temporary/back/app
+
+go 1.23.1
+
+require github.com/google/uuid v1.6.0
---
+++ back/app/internal/ctxkey/ctxkey.go
@@ -0,0 +1,8 @@
+package ctxkey
+
+import "github.com/google/uuid"
+
+type (
+	// UserID - contextに格納する UserID のキー
+	UserID = uuid.UUID
+)
