6942a9c11c633c33f094e16410ea48744b4652be
54m <30588003+54m@users.noreply.github.com>
2024-09-10 07:52:29 +0900

    refactor(back): update repository structure and add Create method
    Replaced the event repository to use infrastructures instead of volcago for better alignment. Added a new Create method to insert events into the repository, including handling potential errors with a custom error message.

--- back/app/internal/repositoryimpl/volcagoimpl/events_repository.go
+++ back/app/internal/repositoryimpl/volcagoimpl/events_repository.go
@@ -8,23 +8,46 @@ import (
 	"github.com/topgate/gcim-temporary/back/app/internal/entities"
 	"github.com/topgate/gcim-temporary/back/app/internal/repositories"
 	"github.com/topgate/gcim-temporary/back/app/internal/repositoryerrors"
-	volcago "github.com/topgate/gcim-temporary/back/app/internal/volcago/repositories"
+	"github.com/topgate/gcim-temporary/back/app/internal/volcago"
+	"github.com/topgate/gcim-temporary/back/app/internal/volcago/infrastructures"
 )
 
 type eventImpl struct {
-	repo volcago.EventRepository
+	infra infrastructures.EventRepository
 }
 
 // NewEvent - Event リポジトリを生成
-func NewEvent(client *firestore.Client) repositories.EventsRepository {
-	return &eventImpl{repo: volcago.NewEventRepository(client)}
+func NewEvent(client *firestore.Client) repositories.BaseRepository[entities.Event] {
+	return &eventImpl{infra: infrastructures.NewEventRepository(client)}
+}
+
+// Create - イベントを作成
+func (e *eventImpl) Create(ctx context.Context, event *entities.Event) error {
+	_, err := e.infra.Insert(ctx, &volcago.Event{
+		ID:           event.ID(),
+		BillingMonth: event.BillingMonth(),
+		Status:       event.Status(),
+		Meta: volcago.Meta{
+			CreatedAt: event.Meta().CreatedAt(),
+			CreatedBy: event.Meta().CreatedBy(),
+			UpdatedAt: event.Meta().UpdatedAt(),
+			UpdatedBy: event.Meta().UpdatedBy(),
+			DeletedAt: event.Meta().DeletedAt(),
+			DeletedBy: event.Meta().DeletedBy(),
+		},
+	})
+	if err != nil {
+		return repositoryerrors.NewUnknownError("failed to create event", err)
+	}
+
+	return nil
 }
 
 // GetByID - ID から取得
 func (e *eventImpl) GetByID(ctx context.Context, id string) (*entities.Event, error) {
-	event, err := e.repo.Get(ctx, id)
+	event, err := e.infra.Get(ctx, id)
 	if err != nil {
-		if errors.Is(err, volcago.ErrNotFound) {
+		if errors.Is(err, infrastructures.ErrNotFound) {
 			return nil, repositoryerrors.NewNotFoundError(err)
 		}
 		return nil, repositoryerrors.NewUnknownError("failed to get event", err)
