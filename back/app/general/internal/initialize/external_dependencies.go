7e9c09d019367c7a2b6788764580ca1abf1c5d59
54m <30588003+54m@users.noreply.github.com>
2024-09-10 08:26:52 +0900

    feat(back): Create external dependencies initializer
    Added a new initializer for external dependencies including Firestore and SES clients as well as OpenAPI specs. This encapsulates the configuration and setup logic in one place, enhancing modularity and maintainability.

---
+++ back/app/general/internal/initialize/external_dependencies.go
@@ -0,0 +1,81 @@
+package initialize
+
+import (
+	"context"
+
+	"cloud.google.com/go/firestore"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ses"
+	"github.com/gcp-kit/gcpen"
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/topgate/gcim-temporary/back/app/general/internal/config"
+	"github.com/topgate/gcim-temporary/back/pkg/environ"
+	"golang.org/x/xerrors"
+)
+
+// ExternalDependencies - external dependencies
+type ExternalDependencies struct {
+	firestoreClient *firestore.Client
+	openapi         *openapi3.T
+	sesService      *ses.SES
+}
+
+// NewExternalDependencies - initialize external dependencies
+func NewExternalDependencies(ctx context.Context, cfg config.Config) (*ExternalDependencies, error) {
+	ed := new(ExternalDependencies)
+	var err error
+
+	{
+		loader := openapi3.NewLoader()
+		spec, err := loader.LoadFromData(OpenAPISpecBin)
+		if err != nil {
+			return nil, xerrors.Errorf("failed to get openapi specs: %w", err)
+		}
+		ed.openapi = spec
+	}
+
+	// AWS
+	{
+		if environ.IsLocal() {
+			sessOpts := session.Options{
+				Config: aws.Config{
+					// リージョンは指定しておく
+					Region: aws.String("ap-northeast-1"),
+				},
+
+				// switch先ロールのprofileを指定
+				Profile: "",
+
+				// MFAトークン取得経路を指定
+				AssumeRoleTokenProvider: stscreds.StdinTokenProvider,
+
+				// 有効にすることで、いい感じに設定が取れるらしい
+				SharedConfigState: session.SharedConfigEnable,
+			}
+			s := session.Must(session.NewSessionWithOptions(sessOpts))
+			sesService := ses.New(s)
+			ed.sesService = sesService
+		}
+	}
+
+	// Google Cloud
+	{
+		projectID := gcpen.ProjectID
+		if environ.IsLocal() {
+			projectID = cfg.FirestoreProjectOnEmulator
+		}
+		ed.firestoreClient, err = firestore.NewClient(ctx, projectID)
+		if err != nil {
+			return nil, xerrors.Errorf("failed to initialize firestore client: %w", err)
+		}
+	}
+
+	return ed, nil
+}
+
+// OpenAPISpec - get openapi specs
+func (e *ExternalDependencies) OpenAPISpec() *openapi3.T {
+	return e.openapi
+}
