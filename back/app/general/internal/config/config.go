441678f04a5cdbd2e23435c7742293b4fe86af6d
54m <30588003+54m@users.noreply.github.com>
2024-09-10 08:26:20 +0900

    feat(back): introduce environment-based configuration
    Add a new configuration file that reads settings from environment variables using confita. This includes settings for CORS, JWT, and Firestore emulator parameters. Default configurations are provided where necessary.

---
+++ back/app/general/internal/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"context"
+
+	"github.com/heetch/confita"
+	"golang.org/x/xerrors"
+)
+
+// Config - config base for reading from outside
+type Config struct {
+	CORSAllowOrigins           string `config:"CORS_ALLOW_ORIGINS"`            // CORS Allow Origins
+	SkipJWT                    bool   `config:"SKIP_JWT"`                      // authentication skip
+	JWTSecret                  string `config:"JWT_SECRET"`                    // JWT生成用のシークレット
+	JWTIssuer                  string `config:"JWT_ISSUER"`                    // JWT生成時のIssuer名
+	FirestoreProjectOnEmulator string `config:"FIRESTORE_PROJECT_ON_EMULATOR"` // on emulator
+}
+
+var defaultConfig = Config{
+	FirestoreProjectOnEmulator: "emulator-projects",
+}
+
+// ReadConfig - read config from environment variables
+func ReadConfig(ctx context.Context) (*Config, error) {
+	loader := confita.NewLoader()
+
+	cfg := defaultConfig
+	if err := loader.Load(ctx, &cfg); err != nil {
+		return nil, xerrors.Errorf("failed to load config: %w", err)
+	}
+
+	return &cfg, nil
+}
