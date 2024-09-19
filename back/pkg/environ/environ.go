a0a43a13d80a89b9bd452137513ad80bad849826
kazunori fujiwara <kazunori.fujiwara.bp@g-gen.co.jp>
2024-09-19 17:41:55 +0900

    add publisher

--- back/pkg/environ/environ.go
+++ back/pkg/environ/environ.go
@@ -6,3 +6,8 @@ import "os"
 func IsLocal() bool {
 	return os.Getenv("IS_LOCAL") == "true"
 }
+
+// ProjectID - プロジェクトID
+func ProjectID() string {
+	return os.Getenv("PROJECT_ID")
+}
---
+++ back/pkg/pubsub/publisher.go
@@ -0,0 +1,39 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"cloud.google.com/go/pubsub"
+	"github.com/topgate/gcim-temporary/back/pkg/environ"
+)
+
+// PublishMsg - メッセージをpubsubに送信する
+func PublishMsg(topicID string, attr map[string]string, msg string) error {
+	projectID := environ.ProjectID()
+
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		return fmt.Errorf("pubsub.NewClient: %v", err)
+	}
+
+	json, err := json.Marshal(msg)
+	if err != nil {
+		return fmt.Errorf("Input msg error : %v", err)
+	}
+
+	t := client.Topic(topicID)
+	result := t.Publish(ctx, &pubsub.Message{
+		Data:       []byte(json),
+		Attributes: attr,
+	})
+
+	_, err = result.Get(ctx)
+	if err != nil {
+		return fmt.Errorf("Get: %v", err)
+	}
+
+	return nil
+}
