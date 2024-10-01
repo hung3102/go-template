51d3b046c051c1c790085e6f0747d7b2736be9a6
n.iwahashi <noriaki.iwahashi.bp@g-gen.co.jp>
2024-10-01 16:49:59 +0900

    mod: PRコメント対応

--- back/app/internal/volcago/org_csp_account_cost.go
+++ back/app/internal/volcago/org_csp_account_cost.go
@@ -4,15 +4,15 @@ package volcago
 
 // OrgCSPAccountCost - 団体ごとのCSPごとのアカウントごと
 type OrgCSPAccountCost struct {
-	ID                   string                          `firestore:"-" firestore_key:""`      // ID
-	EventID              string                          `firestore:"event_id"`                // イベントID
-	GCASProportionCostID string                          `firestore:"gcas_proportion_cost_id"` // GCAS按分コストID
-	GCASAccountCostID    string                          `firestore:"gcas_account_cost_id"`    // GCASアカウントコストID
-	Organization         string                          `firestore:"organization"`            // 団体名
-	CSP                  string                          `firestore:"csp"`                     // CSP
-	AccountID            string                          `firestore:"account_id"`              // アカウントID
-	Cost                 int                             `firestore:"cost"`                    // 金額
-	BillingUnitID        string                          `firestore:"billing_unit_id"`         // 支払い区分ID
-	PaymentAgency        *OrgCSPAccountCostPaymentAgency `firestore:"payment_agency"`          // 支払い代行者情報
+	ID                   string         `firestore:"-" firestore_key:""`      // ID
+	EventID              string         `firestore:"event_id"`                // イベントID
+	GCASProportionCostID string         `firestore:"gcas_proportion_cost_id"` // GCAS按分コストID
+	GCASAccountCostID    string         `firestore:"gcas_account_cost_id"`    // GCASアカウントコストID
+	Organization         string         `firestore:"organization"`            // 団体名
+	CSP                  string         `firestore:"csp"`                     // CSP
+	AccountID            string         `firestore:"account_id"`              // アカウントID
+	Cost                 int            `firestore:"cost"`                    // 金額
+	BillingUnitID        string         `firestore:"billing_unit_id"`         // 支払い区分ID
+	PaymentAgency        *PaymentAgency `firestore:"payment_agency"`          // 支払い代行者情報
 	Meta
 }
--- back/app/internal/volcago/org_csp_account_cost_payment_agency.go
+++
@@ -1,6 +0,0 @@
-package volcago
-
-// OrgCSPAccountCostPaymentAgency - 団体ごとのCSPごとのアカウントごと支払い代行者情報
-type OrgCSPAccountCostPaymentAgency struct {
-	// TODO
-}
